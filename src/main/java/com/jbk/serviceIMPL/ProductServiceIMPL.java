a3b799f425a4afc6b24a1b7c1b03de5518216644
Rohit Kadam <rohitKadm27@gmail.com>
2023-06-14 18:44:19 +0530

    task1

--- src/main/java/com/jbk/serviceIMPL/ProductServiceIMPL.java
+++ src/main/java/com/jbk/serviceIMPL/ProductServiceIMPL.java
@@ -1,6 +1,7 @@
 package com.jbk.serviceIMPL;
 
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,28 +12,34 @@ import com.jbk.service.ProductService;
 @Service
 public class ProductServiceIMPL implements ProductService {
 
-
 	@Autowired
 	private ProductDao dao;
-	
+
 	@Override
 	public boolean saveProduct(Product product) {
 
 		String productId = new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
-		
+
 		product.setProductId(Long.parseLong(productId));
-		
+
 		boolean isAdded = dao.saveProduct(product);
-		
+
 		return true;
 	}
 
 	@Override
 	public List<Product> getAllProducts() {
-        List<Product> list = dao.getAllProducts();
+		List<Product> list = dao.getAllProducts();
 		return list;
 	}
 
-	
-	
+	@Override
+	public Product getMaxPriceProducts() {
+		List<Product> list = dao.getAllProducts();
+		Product product = list.stream().max(Comparator.comparingDouble(Product::getProductPrice)).get();
+		return product ;
+	}
+
+
+
 }
